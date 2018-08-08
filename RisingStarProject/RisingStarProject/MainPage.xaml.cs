8ca730d2ae78da3af9a773e59e50a0fe577e2010
Jacky <JLuo@student.neumont.edu>
2018-08-08 17:56:59 -0600

    Recreted pseudo functions for creation

--- RisingStarProject/RisingStarProject/MainPage.xaml.cs
+++ RisingStarProject/RisingStarProject/MainPage.xaml.cs
@@ -1,9 +1,11 @@
 using RisingStarProject;
+using RisingStarProject.IngedientModel;
 using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
+using System.Text;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
 using Windows.UI.Xaml;
@@ -23,11 +25,26 @@ namespace RisingStarProject
     /// </summary>
     public sealed partial class MainPage : Page
     {
+        private List<Ingredient> ingredients = new List<Ingredient>();
+        private StringBuilder sb = new StringBuilder();
         public MainPage()
         {
             this.InitializeComponent();
         }
 
-
+        private void Create_Ingredients(object sender, RoutedEventArgs e)
+        {
+            //Use string builder to parse info of recipe fields
+            //Store the information into proper fields
+            //Using the variable pass information to create ingredients a new ingredient
+            //Add the ingredient to the ingredients List
+        }
+        private void Create_Recipe(object sender, RoutedEventArgs e)
+        {
+            //Use string builder to parse info of recipe fields
+            //Store the information into proper fields
+            //Using the variable pass information to create ingredients a new recipe
+            //Add the recipe to the recipes List
+        }
     }
 }
