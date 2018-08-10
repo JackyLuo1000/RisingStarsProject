7c6cabe6072a57641ae654e5a0bae6694e0b375a
prborg <ccintron@student.neumont.edu>
2018-08-10 17:29:32 -0600

    Recipe.cs

--- RisingStarProject/RisingStarProject/RecipeModel/Recipe.cs
+++ RisingStarProject/RisingStarProject/RecipeModel/Recipe.cs
@@ -6,7 +6,30 @@ using System.Threading.Tasks;
 
 namespace RisingStarProject.RecipeModel
 {
-    class Recipe
+  class Program
     {
+        public class Recipe
+        {
+            public string Name { get; set; }
+            public string Type { get; set; }
+
+            public Recipe()
+            {
+            }
+
+            public Recipe(string name, string type)
+            {
+                this.Name = name;
+                this.Type = type;
+
+            }
+
+            List<Ingredient> ingredients { get; set; };
+
+            public override string ToString()
+            {
+                return $"Recipe Name: {Name}, Recipe Type: {Type}.";
+            }
+        }
     }
 }
