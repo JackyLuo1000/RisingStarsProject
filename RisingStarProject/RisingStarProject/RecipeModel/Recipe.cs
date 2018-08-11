d3e3e5bcd6747a793b2610d07601166d8881cec1
Jacky <JLuo@student.neumont.edu>
2018-08-10 18:41:25 -0600

    Connect XAML and Code

--- RisingStarProject/RisingStarProject/RecipeModel/Recipe.cs
+++ RisingStarProject/RisingStarProject/RecipeModel/Recipe.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using RisingStarProject.IngedientModel;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,27 +9,47 @@ namespace RisingStarProject.RecipeModel
 {
   class Program
     {
+        [Serializable]
         public class Recipe
         {
+
+            private StringBuilder sb = new StringBuilder();
             public string Name { get; set; }
-            public string Type { get; set; }
+            //public string Type { get; set; }
+            public List<Ingredient> Ingredients { get; set; }
 
             public Recipe()
             {
+                Name = "Recipe";
+                Ingredients = new List<Ingredient>();
             }
 
             public Recipe(string name, string type)
             {
                 this.Name = name;
-                this.Type = type;
+                //this.Type = type;
 
             }
 
-            List<Ingredient> ingredients { get; set; };
 
             public override string ToString()
             {
-                return $"Recipe Name: {Name}, Recipe Type: {Type}.";
+                if (!Name.Equals(""))
+                {
+                    sb.AppendLine($"Recipe Name: {Name}");
+                }
+                //if (!Type.Equals(""))
+                //{
+                //    sb.AppendLine($"Recipe Type: {Name}");
+                //}
+                if(Ingredients.Count() != 0)
+                {
+                    foreach(Ingredient i in Ingredients)
+                    {
+                        sb.AppendLine( i.ToString() );
+                    }
+                }
+                return sb.ToString();
             }
         }
     }
